8aec70b0a7d828904f55e61e988daee91818b66a
amarnathbhat <bhatamar8@gmail.com>
2024-01-17 11:06:25 +0530

    Link Changes

--- Meme Generator/script.js
+++ Meme Generator/script.js
@@ -4,17 +4,20 @@ const memeTitle = document.querySelector(".gen-meme .meme-title");
 const memeAuthor = document.querySelector(".gen-meme .author");
 
 const updateDeets = (url, title, author) => {
-    memeImg.setAttribute("src", url);
-    memeTitle.innerHTML = title;
-    memeAuthor.innerHTML= `Meme by: ${author}`;
-}
+  memeImg.setAttribute("src", url);
+  memeTitle.innerHTML = title;
+  memeAuthor.innerHTML = `Meme by: ${author}`;
+};
 
 // const subreddit= documen.querySelector(".gen-meme .subreddit");
 const genMeme = () => {
-    fetch("https://meme-api.com/gimme").then((response) => response.json().then(data => updateDeets(data.url, data.title, data.author)));
+  fetch("https://meme-api.com/gimme/desimemes").then((response) =>
+    response
+      .json()
+      .then((data) => updateDeets(data.url, data.title, data.author))
+  );
 };
 
 genMemeBtn.addEventListener("click", genMeme);
-
 
 genMeme();
